46503aadfa926bce6c47afea8727b9ec9728437a
Dmitriy <dimakonovalov88@gmail.com>
2023-09-20 18:28:12 +0400

    1.0.2 pre-complete

--- app/app.cc
+++ app/app.cc
@@ -6,17 +6,26 @@
 
 
 int main() {
-	int list[]{ 1, 2};
-	int lsit[]{ 1, -1};
-	auto v1 = Vector<int>(list, 2);
-	auto v2 = Vector<int>(lsit, 2);
-	cout << v1.get_size() << " items:" << endl;;
+	float list[]{ 1, 4};
+	float lsit[]{ 4, 2};
+	auto v1 = Vector<float>(list, 2);
+	auto v2 = Vector<float>(lsit, 2);
+	//auto v3 = Vector<float>(v1);
+	/*cout << v3.get_size() << " items:" << endl;;
+	for (int i(0); i < v3.get_size(); i++) {
+		cout << v3[i] << endl;
+	}
+	v1 = v1 - v2;
 	for (int i(0); i < v1.get_size(); i++) {
 		cout << v1[i] << endl;
-	}
-	//cout << "scalar value = " << scalar(v1, v2) << endl;
+	}*/
+	//cout << "scalar value = " << v1 * v2 << endl;
 	//cout << 1 << endl;
-
 	//cout << cos(v1, v2);
+	auto v3 = bis(v1, v2);
+	cout << v3.get_size() << endl;
+	cout << v3[0] << v3[1];
+
 	return 0;
+
 }
--- include/vector/vector.h
+++ include/vector/vector.h
@@ -3,6 +3,7 @@
 #include <random>
 #include <complex>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -17,6 +18,11 @@ public:
 	Vector(T* list, int size) : axes(new T[size]), size(size) {
 		for (int i(0); i < size; i++) {
 			axes[i] = list[i];
+		}
+	}
+	Vector(Vector& other) : axes(new T[other.size]), size(other.size) {
+		for (int i(0); i < size; i++) {
+			axes[i] = other.axes[i];
 		}
 	}
 	~Vector() {
@@ -43,15 +49,12 @@ public:
 		}
 		return pow(abs, 0.5);
 	}
-	/*double scalar(const Vector<T>& a, const Vector<T>& b) {
-		float scalar = 0;
-		if (a.size == b.size) {
-			for (int i(0); i < a.size; i++) {
-				scalar += (a.axes[i] * b.axes[i]);
-			}
-		}
-		return scalar;
-	}*/
+	double cos(Vector& other) {
+		double cos = ((*this) * other);
+		double scalar = double((*this).abs());
+		scalar *= double(other.abs());
+		return (cos / scalar);
+	}
 
 	Vector& operator=(const Vector& rhs) {
 		if (this != &rhs) {
@@ -92,22 +95,51 @@ public:
 		}
 		return *this;
 	}
-	
-
+	double operator*(const Vector& rhs) {
+		double sum = 0;
+		if (size == rhs.size) {
+			for (int i(0); i < size; i++) {
+				sum += double(axes[i]) * double(rhs.axes[i]);
+			}
+		}
+		return sum;
+	}
+	Vector operator+(Vector<T>& rhs) {
+		auto tmp = Vector<T>(rhs);
+		if (size == rhs.size) {
+			for (int i(0); i < size; i++) {
+				tmp.axes[i] = axes[i] + rhs.axes[i];
+			}
+		}
+		return tmp;
+	}
+	Vector operator-(Vector<T>& rhs) {
+		auto tmp = Vector<T>(rhs);
+		if (size == rhs.size) {
+			for (int i(0); i < size; i++) {
+				tmp.axes[i] = axes[i] - rhs.axes[i];
+			}
+		}
+		return tmp;
+	}
 };
 
-template<typename T>
-double scalar(Vector<T> a, Vector<T> b) {
-	double scalar = 0;
-	if (a.get_size() == b.get_size()) {
-		for (int i(0); i < a.get_size(); i++) {
-			scalar += ((double(a[i])) * (double(b[i])));
-		}
-	}
-	return scalar;
+template <typename T>
+double cos(Vector<T>& a, Vector<T>& b) {
+	double cos = (a * b);
+	double scalar = double(a.abs());
+	scalar *= double(b.abs());
+	return ((a * b) / (double(a.abs()) * double(b.abs())));
 }
 
-template<typename T>
-double cos(Vector<T> a, Vector<T> b) {
-	return (scalar(a, b) / (double(a.abs()) * double(b.abs())));
+template <typename T>
+Vector<T> bis(Vector<T> a, Vector<T> b) {
+	auto a_abs = a.abs();
+	auto b_abs = b.abs();
+	auto x = a_abs / (a_abs + b_abs);
+	auto tmp = b - a;
+	tmp *= x;
+	return tmp + a;
 }
+
+
