b248bd22572a0febcb58ca51bbcb41254853646f
Dimasik322 <dimakonovalov88@gmail.com>
2024-03-31 23:08:24 +0400

    map methods

--- app/app.cc
+++ app/app.cc
@@ -5,12 +5,15 @@ using namespace std;
 
 int main() {
 	Map<int, int> map;
-	map.insert(1, 123124);
-	map.insert(21, 124);
-	map.insert(3, 14);
-	map.insert(6, 3124);
+	map.insert(1, 1);
+	map.insert(21, 2);
+	map.insert(3, 3);
+	map.insert(6, 4);
+	map.insert_or_assign(31, 5);
+	map.insert_or_assign(1, 6);
 	map.print();
-
+	cout << map.erase(61) << endl;
+	map.print();
 	return 0;
 	
 }
--- include/map/map.h
+++ include/map/map.h
@@ -10,9 +10,7 @@ struct Pair {
 	Pair<K,T>* next;
 
 	Pair<K, T>(const K& key, const T& value) : key(key), value(value), next(nullptr) {};
-	Pair<K, T>() {
-
-	}
+	Pair<K, T>() {};
 };
 
 template<typename K, typename T>
@@ -20,34 +18,67 @@ class Map {
 private:
 	size_t _size;
 	Pair<K, T>** _buskets;
-
+	
 	size_t hash(const K& key) const {
 		return key % _size;
+	}
+
+	void copy(const Map<K, T>& other) {
+		for (size_t i(0); i < _size; ++i) {
+			auto tmp = other._buskets[i];
+			while (tmp) {
+				insert(tmp->key, tmp->value);
+				tmp = tmp->next;
+			}
+		}
 	}
 
 public:
 	Map<K, T>() {
 		_size = 10;
 		_buskets = new Pair<K, T>*[_size];
-		for (int i(0); i < _size; ++i) {
+		for (size_t i(0); i < _size; ++i) {
 			_buskets[i] = nullptr;
 		}
 	};
 	Map<K, T>(const size_t size) {
 		_size = size;
-		_buskets = new Pair*[_size];
-		for (int i(0); i < size; ++i) {
+		_buskets = new Pair<K, T>*[_size];
+		for (size_t i(0); i < size; ++i) {
 			_buskets[i] = nullptr;
 		}
 	}
+	Map<K, T>(const Map<K, T>& other) {
+		_size = other._size;
+		_buskets = new Pair<K, T>*[_size];
+		for (size_t i(0); i < _size; ++i) {
+			_buskets[i] = nullptr;
+		}
+		copy(other);
+	}
 	~Map<K, T>() {
 		clear();
-		delete _buskets;
+		delete [] _buskets;
+	}
+
+	Map<K, T>& operator=(const Map<K, T>& other) {
+		if (&other == this) {
+			return *this;
+		}
+		clear();
+		delete [] _buskets;
+		_size = other._size;
+		_buskets = new Pair<K, T>* [_size];
+		for (size_t i(0); i < _size; ++i) {
+			_buskets[i] = nullptr;
+		}
+		copy(other);
+		return *this;
 	}
 
 	void clear() {
 		Pair<K, T>* tmp = nullptr;
-		for (int i(0); i < _size; ++i) {
+		for (size_t i(0); i < _size; ++i) {
 			tmp = _buskets[i];
 			while (tmp) {
 				auto tmp_next = tmp->next;
@@ -56,15 +87,112 @@ public:
 			}
 		}
 	}
+
 	void insert(const K& key, const T& value) {
-		auto new_pair = new Pair<K, T>(key, value);
-		int index = hash(key);
-		new_pair->next = _buskets[index];
-		_buskets[index] = new_pair;
+		size_t index = hash(key);
+		auto tmp = _buskets[index];
+		if (!tmp) {
+			_buskets[index] = new Pair<K, T>(key, value);
+			return;
+		}
+		while (tmp) {
+			if (tmp->key == key) {
+				throw std::invalid_argument("This key already in map");
+			}
+			if (tmp->next) {
+				tmp = tmp->next;
+			}
+			else {
+				tmp->next = new Pair<K, T>(key, value);
+				return;
+			}
+		}
+	}
+	void insert_or_assign(const K& key, const T& value) {
+		size_t index = hash(key);
+		auto tmp = _buskets[index];
+		if (!tmp) {
+			_buskets[index] = new Pair<K, T>(key, value);
+			return;
+		}
+		while (tmp) {
+			if (tmp->key == key) {
+				tmp->value = value;
+				return;
+			}
+			if (tmp->next) {
+				tmp = tmp->next;
+			}
+			else {
+				tmp->next = new Pair<K, T>(key, value);
+				return;
+			}
+		}
+	}
+	bool contains(const T& value) {
+		for (size_t index(0); index < _size; ++index) {
+			auto tmp = _buskets[index];
+			while (tmp) {
+				if (tmp->value == value){
+					return true;
+				}
+				tmp = tmp->next;
+			}
+		}
+		return false;
+	}
+	T* search(const K& key) {
+		size_t index = hash(key);
+		auto tmp = _buskets[index];
+		if (tmp == nullptr) {
+			return nullptr;
+		}
+		while (tmp) {
+			if (tmp->key == key) {
+				return &tmp->value;
+			}
+			tmp = tmp->next;
+		}
+		return nullptr;
+	}
+	bool erase(const K& key) {
+		size_t index = hash(key);
+		auto tmp = _buskets[index];
+		if (tmp == nullptr) {
+			return false;
+		}
+		if (tmp->key == key) {
+			_buskets[index] = tmp->next;
+			delete tmp;
+			return true;
+		}
+		Pair<K, T>* prev = nullptr;
+		while (tmp) {
+			if (tmp->key == key) {
+				prev->next = tmp->next;
+				delete tmp;
+				return true;
+			}
+			prev = tmp;
+			tmp = tmp->next;
+		}
+		return false;
+	}
+	int count(const K& key) {
+		int counter = 0;
+		size_t index = hash(key);
+		auto tmp = _buskets[index];
+		while (tmp) {
+			if (tmp->key == key) {
+				++counter;
+			}
+			tmp = tmp->next;
+		}
+		return counter;
 	}
 	void print() const {
 		Pair<K, T>* tmp = nullptr;
-		for (int i(0); i < _size; ++i) {
+		for (size_t i(0); i < _size; ++i) {
 			tmp = _buskets[i];
 			while (tmp) {
 				std::cout << tmp->key << " : " << tmp->value << std::endl;
